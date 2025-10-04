ad931dda46eaa391338a6c802f7f516a194ac7e5
Rhseynn <137887287+Rhseynn@users.noreply.github.com>
2025-10-04 15:05:31 +0400

    Add files via upload

---
+++ projectEuler/Solutions/014/euler2/Program.cs
@@ -0,0 +1,32 @@
+﻿
+        long maxNumber = 0;        // Ən çox əməliyyat tələb edən ədəd
+        long maxOperations = 0;    // Əməliyyatların maksimum sayı
+
+        for (int i = 1; i <= 1_000_000; i++)
+        {
+            long number = i;
+            long operations = 0;
+
+            while (number != 1)
+            {
+                if (number % 2 == 0)
+                {
+                    number /= 2; 
+                }
+                else
+                {
+                    number = number * 3 + 1; 
+                }
+
+                operations++;
+            }
+
+            if (operations > maxOperations)
+            {
+                maxOperations = operations;
+                maxNumber = i;
+            }
+        }
+
+        Console.WriteLine($"Ən çox əməliyyat edən ədəd: {maxNumber}");
+        Console.WriteLine($"Əməliyyatların sayı: {maxOperations}");
